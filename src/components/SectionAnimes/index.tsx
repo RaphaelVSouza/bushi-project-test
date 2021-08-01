6b8175fddd6a387bddb427503cf728b52f59bb3d
Raphael Vieira <raphael5254@outlook.com>
2021-08-01 14:47:59 -0300

    remove unused var

--- src/components/SectionAnimes/index.tsx
+++ src/components/SectionAnimes/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { animesFetchData } from 'store/actions/fetchAnimes';
 import { connect } from 'react-redux';
 //import AnimeCard from 'components/AnimeCard'
