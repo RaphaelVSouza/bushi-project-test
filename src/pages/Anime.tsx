a7d409faf4014a9104f5393a6efcd63565cf386d
Raphael Vieira <raphael5254@outlook.com>
2021-08-01 23:24:53 -0300

    fix: redirect if access /anime without anime

--- src/pages/Anime.tsx
+++ src/pages/Anime.tsx
@@ -1,9 +1,14 @@
+import { useHistory } from 'react-router-dom'
 import Footer from "components/Footer"
 import Header from "components/Header"
 import SectionAnimeDetails from "components/SectionAnimeDetails"
 
 function Anime({ location }: any) {
+  const history = useHistory()
+  if(!location.state) history.push('/')
+
   const {title, synopsis, posterImage} = location.state.animeDetails;
+
   window.scrollTo(0, 0);
   
   return (
